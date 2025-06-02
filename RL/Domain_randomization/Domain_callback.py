e2b284d7e393b5859d503f476c94186b1f3a7d05
edheadd <ebexie@gmail.com>
2025-06-02 15:17:57 -0400

    added publisher

--- RL/Domain_randomization/Domain_callback.py
+++ RL/Domain_randomization/Domain_callback.py
@@ -1,14 +1,16 @@
 from stable_baselines3.common.callbacks import BaseCallback
+import rospy
+from ambf_msgs.msg import WorldCmd
 
 class DomainRandomizationCallback(BaseCallback):
-    def __init__(self, model, randomization_params, verbose=0):
+    def __init__(self, randomization_params, verbose=0):
         super().__init__(verbose)
-        self.env = model.env
+        
         self.randomization_params = [True if x == "1" else False for x in randomization_params.split(",")]
         print("Randomization params: ", self.randomization_params)
-        self.camera_randomization = self.randomization_params[0]
-        self.light_randomization = self.randomization_params[1]
-        self.psm_randomization = self.randomization_params[2]
+        self.randomize_gravity = self.randomization_params[0]
+        
+        self.pub = rospy.Publisher('/WorldRandomization/Commands/Command', WorldCmd, queue_size=10)
 
     def _on_rollout_start(self):
         print("Randomizing AMBF (Rollout start)")
@@ -22,7 +24,15 @@ class DomainRandomizationCallback(BaseCallback):
         return True
     
     def randomize(self):
-        return;
+        
+        msg = WorldCmd()
+        msg.randomize_gravity = self.randomize_gravity
+        if self.randomize_gravity:
+            msg.gravity.x = 0
+            msg.gravity.y = 0
+            msg.gravity.z = -9.81
+        self.pub.publish(msg)
+        print("Published randomization command")
 
 
 
